42da38e4de45a68a0b3b4c3a4b4b645e178994bf
root <chimachike1@gmail.com>
2024-01-16 18:43:18 +0000

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+method that calculates the fewest number of operations
+"""
+def minOperations(n):
+    """
+    a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    """
+
+    run = 1
+    start = 0
+    count = 0
+    while run < n:
+        remainder = n - run
+        if (remainder % run == 0):
+            start = run
+            run += start
+            count += 2
+        else:
+            run += start
+            count += 1
+    return count
